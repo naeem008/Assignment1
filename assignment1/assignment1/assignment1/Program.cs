b44795bef143e7568b58b87a5f4a2f071a4de41f
naeem008 <zannatnaeem3@gmail.com>
2019-09-10 20:23:32 +0600

    add_project_ignore

---
+++ assignment1/assignment1/assignment1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name,age,address,contact;
+            Console.WriteLine("Enter your name");
+            
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            
+            age = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            
+            address = Console.ReadLine();
+            Console.WriteLine("Enter your contact");
+            contact = Console.ReadLine();
+            
+
+            Console.WriteLine("Name:"+ name);
+            Console.WriteLine("Address:" + address);
+            Console.WriteLine("Age:"+ age);
+            Console.WriteLine("Contact:" + contact);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
